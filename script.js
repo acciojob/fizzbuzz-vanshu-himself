ae7e30c327ae3f50c55d3876009d3a0bf3e1dfdd
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-05-26 15:26:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ let str="";
+for(let i=1;i<=100;i++){
+	if(i%3==0){
+		str+="Fizz\n"
+	}else if(i%5==0){
+		str+="Buzz\n"
+	}else if(i%3==0 && i%5==0){
+		str+="FizzBuzz\n"
+	}else{
+		str+=i+"\n";
+	}
+}alert(str);
+	
